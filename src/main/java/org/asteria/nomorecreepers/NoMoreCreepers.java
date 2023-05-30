4dd95de013313c7f4ecce950865e8854dc17a621
Star <30036225@students.southwales.ac.uk>
2023-05-30 15:49:40 +0100

    Version 2
    Added features

--- src/main/java/org/asteria/nomorecreepers/NoMoreCreepers.java
+++ src/main/java/org/asteria/nomorecreepers/NoMoreCreepers.java
@@ -1,9 +1,13 @@
 package org.asteria.nomorecreepers;
 
+import com.sun.org.apache.bcel.internal.generic.Select;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -35,13 +39,39 @@ public final class NoMoreCreepers extends JavaPlugin implements Listener {
         e.setCancelled(!canSpawn);      //if it cant spawn, cancel the spawn event
 
         if (canSpawn) {     //If the entity is spawned, there may be additional events depending on the type.
-            //do nothing
-        }
-        else{
+            switch(e.getEntityType()) {
+                case SKELETON:  //the entity is a skeleton
+                    Skeleton S = (Skeleton) e.getEntity();
+                    if (!getConfig().getBoolean("SKELETON.BOW")) {
+                        S.getEquipment().setItemInMainHand(null);
+                    }
+                case ZOMBIE:        //the entity is a skeleton
+                    if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.REINFORCEMENTS) {      //tests if the entity is spawned because of a reinforcement call
+                        e.setCancelled(!getConfig().getBoolean("ZOMBIE.REINFORCEMENTS"));      // checks the config file to see if reinforcments are allowed
+                    }
+
+            }
 
 
         }
     }
+
+    @EventHandler
+    public void OnCreeperExplode(ExplosionPrimeEvent e) {
+        if (e.getEntityType() == EntityType.CREEPER) {
+
+            e.setCancelled(!getConfig().getBoolean("CREEPER.EXPLODES"));
+        }
+    }
+    @EventHandler
+    public void OnTeleport(EntityTeleportEvent e) {
+        if (e.getEntityType() == EntityType.ENDERMAN) {
+            e.setCancelled(!getConfig().getBoolean("ENDERMAN.TELEPORT"));
+        }
+    }
+
+
+
 
 
     @Override
