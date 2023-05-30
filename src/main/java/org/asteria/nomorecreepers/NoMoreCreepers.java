64b99c2f5becc2c41f6db877c26a680df7ddf272
Star <30036225@students.southwales.ac.uk>
2023-05-30 16:56:36 +0100

    Added fun features
    -Johnny vindicators
    -1/1000 giants
    -Martyrdom
    -Rainbow Sheep

--- src/main/java/org/asteria/nomorecreepers/NoMoreCreepers.java
+++ src/main/java/org/asteria/nomorecreepers/NoMoreCreepers.java
@@ -1,19 +1,25 @@
 package org.asteria.nomorecreepers;
 
-import com.sun.org.apache.bcel.internal.generic.Select;
+
 import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTeleportEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Random;
+
 
 public final class NoMoreCreepers extends JavaPlugin implements Listener {
 
-    Creatures MobHolder[];
+    Random rand;
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -22,16 +28,28 @@ public final class NoMoreCreepers extends JavaPlugin implements Listener {
         //Tell events to check here
         Bukkit.getPluginManager().registerEvents(this, this);   //(Class that handles events, self)
 
+        rand = new Random();
     }
 
+    @EventHandler
+    public void OnCreatureDeath(EntityDeathEvent e) {
+        if (e.getEntityType() == EntityType.CREEPER && getConfig().getBoolean("FUN.MARTYRDOM")) {
+            Creeper C = (Creeper) e.getEntity();
+            C.explode();
+        } else if (e.getEntityType() == EntityType.GIANT) {
+            Giant G = (Giant) e.getEntity();
+            Zombie Z = (Zombie) G.getPassengers().get(0);
+            Z.remove();
+        }
 
+    }
     @EventHandler
     public void OnCreatureSpawn(CreatureSpawnEvent e) {
 
         boolean canSpawn;       //Holds whether the entity is allowed to spawn
 
-        //Essentially a XAND gate here (Equivelent to a NXOR)
-        canSpawn = !(getConfig().contains(e.getEntity().getName().toUpperCase()) ^ getConfig().getBoolean(e.getEntity().getName().toUpperCase()+".SPAWN"));
+        //Essentially a XAND gate here (Equivalent to a NXOR)
+        canSpawn = !(getConfig().contains("SPAWN-PREVENTION."+e.getEntity().getName().toUpperCase()) ^ getConfig().getBoolean("SPAWN-PREVENTION." + e.getEntity().getName().toUpperCase()+".SPAWN"));
         //First checks if the entity is mentioned inside the config.yml,
         //Then checks the rule of its spawning inside the config
 
@@ -40,16 +58,48 @@ public final class NoMoreCreepers extends JavaPlugin implements Listener {
 
         if (canSpawn) {     //If the entity is spawned, there may be additional events depending on the type.
             switch(e.getEntityType()) {
-                case SKELETON:  //the entity is a skeleton
-                    Skeleton S = (Skeleton) e.getEntity();
-                    if (!getConfig().getBoolean("SKELETON.BOW")) {
+                case SKELETON:  //the entity is a skeleton#
+                {
+
+                    if (!getConfig().getBoolean("ADDITIONAL-FEATURES.SKELETON-BOWS")) {
+                        Skeleton S = (Skeleton) e.getEntity();
                         S.getEquipment().setItemInMainHand(null);
                     }
+                    break;
+                }
                 case ZOMBIE:        //the entity is a skeleton
+                {
                     if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.REINFORCEMENTS) {      //tests if the entity is spawned because of a reinforcement call
-                        e.setCancelled(!getConfig().getBoolean("ZOMBIE.REINFORCEMENTS"));      // checks the config file to see if reinforcments are allowed
+                        e.setCancelled(!getConfig().getBoolean("ADDITIONAL-FEATURES.REINFORCEMENTS"));      // checks the config file to see if reinforcements are allowed
                     }
-
+                    if (getConfig().getBoolean("FUN.SPAWN-GIANTS")) {
+                        if (rand.nextInt(0,1000) == 1) {
+                            Location temp = e.getLocation();
+                            //e.setCancelled(true);
+                            Giant G = (Giant) Bukkit.getWorld("world").spawnEntity(temp, EntityType.GIANT);
+                            G.addPassenger(e.getEntity());
+                            //e.getEntity().setInvisible(true);
+                            e.getEntity().setInvulnerable(true);
+                            //when the giant dies, I need to call back and kill its passenger
+                            G.setAI(true);
+                            //They can now move but have no attack, ill see what I can do about this when I am smarter
+                        }
+                    }
+                    break;
+                }
+                case SHEEP:
+                {
+                    if (getConfig().getBoolean("FUN.RAINBOW-SHEEP")) {
+                        Sheep S = (Sheep) e.getEntity();
+                        S.setColor(DyeColor.MAGENTA);
+                        S.setCustomName("jeb_");
+                    }
+                    break;
+                }
+                case VINDICATOR: {
+                    Vindicator V = (Vindicator) e.getEntity();
+                    V.setJohnny(getConfig().getBoolean("FUN.HERES-JOHNNY"));
+                }
             }
 
 
@@ -60,13 +110,13 @@ public final class NoMoreCreepers extends JavaPlugin implements Listener {
     public void OnCreeperExplode(ExplosionPrimeEvent e) {
         if (e.getEntityType() == EntityType.CREEPER) {
 
-            e.setCancelled(!getConfig().getBoolean("CREEPER.EXPLODES"));
+            e.setCancelled(!getConfig().getBoolean("ADDITIONAL-FEATURES.CREEPERS-EXPLODE"));
         }
     }
     @EventHandler
     public void OnTeleport(EntityTeleportEvent e) {
         if (e.getEntityType() == EntityType.ENDERMAN) {
-            e.setCancelled(!getConfig().getBoolean("ENDERMAN.TELEPORT"));
+            e.setCancelled(!getConfig().getBoolean("ADDITIONAL-FEATURES.ENDERMAN-TELEPORT"));
         }
     }
 
